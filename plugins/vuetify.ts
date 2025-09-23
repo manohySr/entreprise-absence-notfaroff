42da36b0de6697fa261cde2e5256ab582a44ba8e
manohySr <manohyrajaonah@gmail.com>
2025-09-23 16:15:01 +0300

    init project
    install dependencies for nuxt3 and vuetify

---
+++ plugins/vuetify.ts
@@ -0,0 +1,16 @@
+// import this after install `@mdi/font` package
+import "@mdi/font/css/materialdesignicons.css";
+
+import "vuetify/styles";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { createVuetify } from "vuetify";
+
+export default defineNuxtPlugin((app) => {
+  const vuetify = createVuetify({
+    // ... your configuration
+    components,
+    directives,
+  });
+  app.vueApp.use(vuetify);
+});
