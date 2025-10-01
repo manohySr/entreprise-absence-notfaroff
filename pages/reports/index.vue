ab7516bf99f13bec7786b697fea0c989724779d6
manohySr <manohyrajaonah@gmail.com>
2025-10-01 19:56:57 +0300

    add: 404 for settings and reports
    used for making it more like webapp

---
+++ pages/reports/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="d-flex flex-column align-center justify-center" style="min-height: 60vh">
+    <v-icon size="100" color="grey-lighten-1">mdi-alert-circle-outline</v-icon>
+    <h1 class="text-h2 font-weight-bold mt-4 mb-2">404</h1>
+    <p class="text-h5 text-grey mb-4">Page Not Found</p>
+    <p class="text-body-1 text-grey mb-6">The Reports feature is coming soon!</p>
+    <v-btn color="primary" to="/" prepend-icon="mdi-home">
+      Back to Dashboard
+    </v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+// This page will be implemented in the future
+</script>
